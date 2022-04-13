dc4aec98935d1df65ba7244feaed0ef31d269969
Himalaya <yugmani@yahoo.com>
2022-04-12 17:16:46 -0700

    modified fetch nasa api call

--- objects-nasa/js/main.js
+++ objects-nasa/js/main.js
@@ -8,39 +8,27 @@ const description = document.getElementById("description");
 window.onload = fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasa_key}`)
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
     title.innerText = data.title;
     day.innerText = data.date;
     imgEl.src = data.url;
     description.innerText = data.explanation;
-    //   document.querySelector(".display-section").style.display = "block";
-    //   document.querySelector(".input-section").style.display = "none";
   })
   .catch((err) => console.log(err));
 
-let start = "",
-  end = "";
 const btnEl = document.querySelector("button");
 btnEl.addEventListener("click", (e) => {
   e.preventDefault();
-  start = document.querySelector("input").value;
-  end = start;
+  const start = document.querySelector("input").value;
 
-  fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=${nasa_key}&start_date=${start}&end_date=${end}`
-  )
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasa_key}&date=${start}`)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      title.innerText = data[0].title;
-      day.innerText = data[0].date;
-      imgEl.src = data[0].url;
-      description.innerText = data[0].explanation;
+      title.innerText = data.title;
+      day.innerText = data.date;
+      imgEl.src = data.url;
+      description.innerText = data.explanation;
       document.querySelector("input").value = "";
-      //   document.querySelector(".display-section").style.display = "block";
-      //   document.querySelector(".input-section").style.display = "none";
     })
     .catch((err) => console.log(err));
 });
-
-// console.log(nasa_day);
